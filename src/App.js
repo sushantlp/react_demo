a90c8783762ced01a25e50605ad65d8b7a3f6467
sushantlp <sushantsingh.1081@gmail.com>
2020-05-29 17:07:04 +0530

    dashboard login added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { Container } from "semantic-ui-react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Done from "./components/auth/Done";
+import Dashboard from "./components/dashboard/Dashboard";
 
-import Login from "./Login";
-
-const App = () => (
-  <Container>
-    <Login />
-  </Container>
-);
-
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/done" component={Done} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Router>
+    );
+  }
+}
 export default App;
